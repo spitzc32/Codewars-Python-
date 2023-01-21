a98d5453926eb56ccfaacf15f3615c8b786d9c88
Jayra Ortiz <jayragaile@gmail.com>
2023-01-21 23:14:12 +0800

    Added another solution to discover original price

--- 7kyu/7kyu Discover Original Price.py
+++ 7kyu/7kyu Discover Original Price.py
@@ -1,2 +1,7 @@
 def discover_original_price(discounted_price, sale_percentage):
     return round(discounted_price / ((100 - sale_percentage) * 0.01), 2)
+
+# Another solution
+def discover_original_price(discounted_price, sale_percentage):
+    sale_per_dec = float('.' + str(sale_percentage).replace('.','')) if sale_percentage > 10 else float('.0' + str(sale_percentage).replace('.','')) 
+    return round(discounted_price / (1 - sale_per_dec), 2)
